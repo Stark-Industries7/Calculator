fdaec2e1ffd10336a5ba851773b9dc2d8bc10dad
Stark-Industries7 <apooravmukherjee@gmail.com>
2024-06-11 16:18:22 +0530

    Code completed and all changes commited

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("----------------Welcome to the state of the art calculator----------------")
+    print("Please state your equation")
+    equation = eval(input())
+    print(equation)
+    print("Thanks for using this calculator!!")
+
+if __name__ == "__main__":
+    main()    
