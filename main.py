4fabb0eee39d183d803195380976d45469266d10
Stark-Industries7 <apooravmukherjee@gmail.com>
2024-06-29 04:28:09 +0530

    somethings changed in the files

--- main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("----------------Welcome to the state of the art calculator----------------")
-    print("Please state your equation")
-    equation = eval(input())
-    print(equation)
-    print("Thanks for using this calculator!!")
-
-if __name__ == "__main__":
-    main()    
